9e1b43a4ba7918e5d2a388a182688d8f6cb1e2d0
Nina Galindo <ninalorettagalindo@gmail.com>
2026-07-07 22:02:32 +0900

    added the form to write a new reivew

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, numericality: {
-    greater_than_or_equal_to: 0,
+    greater_than_or_equal_to: 1,
     less_than_or_equal_to: 5
   }
 end
